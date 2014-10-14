f7726d8a39bea7d8adb222c8883df3fbdfda5d2e
logger <logger@BJWS-28-75.(none)>
2014-10-14 11:52:12 +0800

    update

--- tasks/logger.js
+++ tasks/logger.js
@@ -1,38 +1,58 @@
 var moment = require('moment');
+var mt = require('microtime');
 var _ = require('lodash');
+var cs = require('../lib/console');
+var fs = require('fs');
+var csv = require('fast-csv');
 var reader = require('../lib/reader');
 var env = process.env.NODE_ENV;
 var eacher = reader.eacher();
 var timingDb = {};
-var limit = 10;
+var limit = 10000;
 var count = 0;
 var showKeys = {
     rt: [],
     ol: ['la'],
     ax: ['open', 'received', 'loading', 'done'],
 }
+var startTime = mt.now();
+var endTime;
+
+var csvStream = csv.format({headers: true}),
+    writableStream = fs.createWriteStream("my.csv");
+
+writableStream.on("finish", function(){
+   console.log("DONE!");
+});
+csvStream.pipe(writableStream);
+
 eacher(function (data) {
-    if (count > limit) return false;
-    calc(data);
+    if (limit && count > limit) return false;
+    transfer(data);
     count++;
 }).then(function () {
-    console.log(timingDb);
+    endTime = mt.now();
+    cs.info('read use time: ' + (endTime - startTime));
+    cs.info(count + ' line logs');
+    calc();
+    csvStream.end();
+    cs.info('use time: ' + (mt.now() - startTime));
 });
-function calc(data) {
+function transfer(data) {
     var type = data.tp;
     switch (type) {
     case 'ax':
-        transfer(data, 'url');
+        //transfer(data, 'url');
         break;
     case 'rt':
-        transfer(data, 'rt');
+        transferRt(data, 'rt');
         break;
     case 'ol':
-        transfer(data, 'pid');
+        //transfer(data, 'pid');
         break
     }
 }
-function transfer(data, type) {
+function transferOl(data, type) {
     var tp = data.tp;
     var sks = showKeys[tp] || [];
     var indexDb = timingDb[tp] = timingDb[tp] || {};
@@ -42,8 +62,8 @@ function transfer(data, type) {
 
     _.forEach(data, function (v, k) {
         if (k.indexOf('rt_') === 0) {
-            timing.rt[k] = timingDb.rt[k] || [];
-            timingDb.push(v);
+            timingDb.rt[k] = timingDb.rt[k] || [];
+            timingDb.rt[k].push(v);
         } else {
             i = _.indexOf(sks, k)
             if (i < 0) return;
@@ -52,13 +72,53 @@ function transfer(data, type) {
         }
     });
 } 
-function calcOl(data) {
-    toDbBy(data, 'pid');
+function transferRt(data, type) {
+    var tp = data.tp;
+    var idb = timingDb[tp] = timingDb[tp] || {};
+    _.forEach(data, function (v, k) {
+        if (k.indexOf('rt_') !== 0) {
+            return;
+        }
+        idb[k] = idb[k] || []; 
+        idb[k].push(v); 
+    });
 }
-function calcRt(data) {
+function calc() {
+    calcRt(0.9);
+    calcRt(0.5);
 }
-function calcAx(data) {
-    toDbBy(data, 'url');
+function calcOl() {
+}
+function calcRt(per) {
+    per = per || 0.5;
+    var data = timingDb.rt;
+    var len;
+    var startIndex; 
+    var endIndex; 
+    var result = {};
+    var tmpData;
+    _.forEach(data, function (v, k) {
+        len = v.length;
+        startIndex = len * (per - 0.1); 
+        endIndex = len * per; 
+        data[k] = v.sort(function (v1, v2) {
+            return v1 - v2;
+        });
+        tmpData = data[k].slice(startIndex, endIndex);
+        result[k] = {};
+        result[k].count = len;
+        result[k].data = tmpData;
+        result[k].samplecount = tmpData.length;
+    })
+    _.forEach(result, function (v, k) {
+        var totalTime = 0;
+        _.forEach(v.data, function (v) {
+            totalTime += (+v);
+        });
+        csvStream.write({timingLabel: k, average: (totalTime / v.samplecount).toFixed(0), percent: per * 100  + '%'});
+    });
+}
+function calcAx() {
 }
 function limiter(task) {
 }
