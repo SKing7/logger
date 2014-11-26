22d33ff10b23c59b2da4bee0c0573c563dc3c5e3
liuzhe <zhe.liu@autonavi.com>
2014-11-26 20:19:50 +0800

    reset test code

--- scripts/mail.sh
+++ scripts/mail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/logger/analyzer
-/usr/local/bin/node tasks/dailyreport.js
+NODE_ENV=production /usr/local/bin/node tasks/dailyreport.js
 node tasks/pagespeed.js -o page -n '/index/index/'
 node tasks/pagespeed.js -o page -n '/search/view/'
 node tasks/pagespeed.js -o page -n '/search/mapview/'
