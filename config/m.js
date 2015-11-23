b591cdb5b371b739884b018dd3946d6342100783
sking <sking.pt@gmail.com>
2015-11-23 18:21:55 +0800

    结构化 配置

---
+++ config/m.js
@@ -0,0 +1,113 @@
+var path = require('path');
+var rtMap = ['rt_index_index', 'rt_search_view', 'rt_search_mapview', 'rt_detail_index', 'rt_navigation_index', 'rt_navigation_buslist', 'rt_nearby_index'];
+var olMap = ['/index/index/', '/search/view/', '/search/mapview/', '/detail/index/', '/navigation/index/', '/navigation/buslist/', '/nearby/index/'];
+var imMap = ['/index/index/', '/search/view/', '/search/mapview/', '/detail/index/', '/navigation/index/', '/navigation/buslist/', '/nearby/index/'];
+var axMap = ['/service/poi/keywords.json', '/service/valueadded/infosearch.json'];
+var ssrTiming = ['c_fsp','fsp', 'ssrct', 'fsp_mixed'];
+var olTiming = ['la', 'dl']; //
+var netTiming = [ 'readyStart', 'redirectTime', 'requestTime', 'initDomTreeTime', 'domReadyTime'];
+var axTiming = ['total', 'received', 'done'];
+var imTiming = [
+'t_loc_android', 'loc_android_ip_failure','loc_android_other_failure',
+'loc_android_glh_success',  'loc_android_ugi_success','loc_android_ams_success',
+'t_loc_ios', 'loc_ios_ip_failure', 'loc_ios_other_failure', 
+'loc_ios_glh_success', 'loc_ios_ugi_success',
+//'loc_ios_ams_success',
+];
+var exTiming = ['cchr'];
+
+module.exports = {
+    aid : 1,
+    showInChart: ['c_fsp', 'dl', 'fs', 'la', 'received', 'done', 'total'],
+    mail: {
+		title: '[速度报表]高德M站速度指标报表_',
+        templatePath: path.resolve(__dirname + '/../templates') + '/',
+        to: process.env.NODE_ENV === 'production' ? 'amap-web-m@list.alibaba-inc.com' : 'liuzhe.pt@alibaba-inc.com',
+        cc: process.env.NODE_ENV === 'production' ? 'tongyao.ty@alibaba-inc.com,shaohang.ysh@alibaba-inc.com,yuki@alibaba-inc.com,sunzhao.szh@alibaba-inc.com,wangxing.wangx@alibaba-inc.com,cuifang.gcf@alibaba-inc.com,liuzhe.pt@alibaba-inc.com': '',
+        transport: {
+            host: 'smtp.alibaba-inc.com',
+            secure: true,
+            port: 465,
+            tls: {
+                rejectUnauthorized: false
+            },
+            auth: {
+                user: 'opendev_noreply@alibaba-inc.com',
+            }
+        }
+    },
+	limit: {
+        keyMap: {
+            ol: olMap,
+            im: imMap,
+        },
+        timingKeyMap: {
+            ol: olTiming.concat(ssrTiming, netTiming),
+            ax: axTiming,
+        },
+	},
+	order: {
+	    timingTypesOrder: ['c_fsp', 'fs'].concat(ssrTiming.slice(1), exTiming, imTiming, olTiming, axTiming, netTiming),
+	    keysOrder:  olMap.concat(rtMap, axMap) 
+	},
+    higherBetter: ['cchr'],
+    timingValueIsPercent: exTiming,
+    alias: {
+		timingKeyMap: {
+        	la: 'Page Load Time',
+        	fsp: 'SSR First Paint Screen Time',
+        	ssrct: 'SSR Server Cost Time',
+        	fsp_mixed: 'SSR Total Cost Time',
+        	c_fsp: 'Timing Marks(with SSR)',
+        	dl: 'Page DOMContent Load Time',
+        	received: 'API Wait Time',
+        	done: 'API Receive & Parse Time',
+        	total: 'API Total Time',
+        	fs: 'Timing Marks',
+        	cchr: 'Static File Cache Hit Rate',
+            loc_ios_ip_failure: 'iOS IP Location Failure',
+            loc_ios_other_failure: 'iOS Other Location Failure',
+            loc_android_ip_failure: 'Android IP Location Failure',
+            loc_android_other_failure: 'Android Other Location Failure',
+            loc_android_glh_success: 'Android HTML5 GeoLocaltion Success Rate' , 
+            loc_ios_glh_success: 'iOS HTML5 GeoLocaltion Success Rate', 
+            loc_android_ugi_success: 'Android UC GI Location Success Rate',
+            loc_ios_ugi_success: 'iOS UC GI Location Success Rate', 
+            loc_android_ams_success: 'Android AMP Service Location Success Rate',
+            //loc_ios_ams_success: 'iOS AMP Service Location Success Rate',
+        	t_loc_ios: 'iOS Location Cost Time',
+        	t_loc_android: 'Android Location Cost time',
+            readyStart: 'Browser Fetch Start Time',
+            redirectTime: 'Page Redirect Time',
+            requestTime: 'Page Download Time',
+            initDomTreeTime: 'DOM Tree Init Time',
+            domReadyTime: 'DOM Tree Init-Ready Time',
+		},
+        /*
+        TODO  #too ugly
+        */
+		keyMap: {
+			'rt_index_index'        : '首页首屏',
+			'rt_nearby_index'       : '附近页首屏',
+			'rt_search_view'        : '搜索结果列表页首屏',
+			'rt_search_mapview'     : '搜索结果图面页首屏',
+			'rt_detail_index'       : 'POI详情页首屏',
+			'rt_navigation_index'   : '路线首页首屏',
+			'rt_navigation_buslist' : '公交导航结果页首屏',
+
+			'/index/index/'         : '首页',
+			'/nearby/index/'         : '附近页',
+			'/search/view/'         : '搜索结果列表页',
+			'/search/mapview/'      : '搜索结果图面页',
+			'/detail/index/'        : 'POI详情页',
+			'/navigation/index/'    : '路线首页',
+			'/navigation/buslist/'  : '公交导航结果页',
+
+			'/service/poi/keywords.json'    : '搜索数据',
+			'/service/valueadded/infosearch.json'  : 'POI详情数据',
+		},
+    },
+    pers: [0.05,0.15,0.25,0.35,0.45,0.55,0.65,0.75,0.85, 0.95, 1],
+    primaryPer: 0.75,
+    screenShotImgs: [ 'index_index', 'detail_index', 'navigation_index', 'navigation_buslist', 'search_view', 'search_mapview']
+}
--- controller/quartile.js
+++ controller/quartile.js
@@ -7,6 +7,7 @@ var config = require('../config/config');
 var cs = require('../lib/console');
 var moment = require('moment');
 var logConfig = config.log;
+var aid = config.aid;
 var Quartile = require('../models/quartile'),
     _ = require('lodash');
 
@@ -19,34 +20,40 @@ exports.create = function (data) {
     });
 };
 exports.find = function (filter, cb) {
+    filter = mixedFilter(filter);
     Quartile.find(filter).sort('reportDate').exec(cb);
 };
-exports.deleteByDate = function (d, cb) {
-    var filter = {reportDate: oneDay(d)}; 
+exports.delete = function (filter, cb) {
+    filter = mixedFilter(filter);
     Quartile.remove(filter).exec(cb);
+};
+exports.deleteByDate = function (d, cb) {
+    var filter = {}; 
+    var filter = mixedFilter({reportDate: oneDay(d) });
+    this.delete(filter, cb);
 };
 exports.findAtThatDay = function (d, cb) {
     var filter = {reportDate: oneDay(d)}; 
-    Quartile.find(filter).exec(cb);
+    this.find(filter, cb);
 };
 exports.findAtPreMonth = function (d, cb) {
     var tmp = moment(d, logConfig.timestampRegx).subtract(1, 'month').format(logConfig.timestampRegx);
     var filter = {reportDate: oneDay(tmp)}; 
-    Quartile.find(filter).exec(cb);
+    this.find(filter, cb);
 };
 exports.findAtPreWeek = function (d, cb) {
     var tmp = moment(d, logConfig.timestampRegx).subtract(7, 'day').format(logConfig.timestampRegx);
     var filter = {reportDate: oneDay(tmp)}; 
-    Quartile.find(filter).exec(cb);
+    this.find(filter, cb);
 };
 exports.findAtPreDay = function (d, cb) {
     var tmp = moment(d, logConfig.timestampRegx).subtract(1, 'day').format(logConfig.timestampRegx);
     var filter = {reportDate: oneDay(tmp)}; 
-    Quartile.find(filter).exec(cb);
+    this.find(filter, cb);
 };
 exports.find7Days = function (d, cb) {
     var filter = {reportDate: recent7Days(d)}; 
-    Quartile.find(filter).exec(cb);
+    this.find(filter, cb);
 };
 exports.findPre7Days = function (d, cb) {
     var start = moment(d, logConfig.timestampRegx).subtract(13, 'day').subtract(1, 'second');
@@ -54,7 +61,7 @@ exports.findPre7Days = function (d, cb) {
     var filter = {
 		reportDate: {$gte: start, $lte: end}
 	};
-    Quartile.find(filter).exec(cb);
+    this.find(filter, cb);
 };
 function oneDay(d) {
     var start = moment(d, logConfig.timestampRegx).subtract(1, 'second');
@@ -65,4 +72,7 @@ function recent7Days(d) {
     var start = moment(d, logConfig.timestampRegx).subtract(6, 'day').subtract(1, 'second');
     var end = moment(d, logConfig.timestampRegx).subtract(1, 'second').add(1, 'day');
     return {$gte: start, $lte: end}; 
+}
+function mixedFilter(ext) {
+    return _.assign({aid: aid}, ext);
 }
